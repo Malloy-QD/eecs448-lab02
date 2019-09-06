e5a14032d639e7146e3003c78309f0dcaad33928
Qingyang Dong <q527d476@ku.edu>
2019-09-06 11:16:27 -0500

    Fix the size method by returning m_size instead of returning 0

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -32,7 +32,8 @@ int LinkedList<T>::size() const
 	/** TODO 
 		Fix this method
 	*/
-	return(0);
+
+	return(m_size);
 }
 
 template <typename T>
@@ -40,7 +41,9 @@ bool LinkedList<T>::search(T value) const
 {
 	Node<T>* temp = m_front;
 	bool isFound = false;
+for(int i=0;i<m_size;i++){
 
+}
 	/** TODO 
 		Fix this method
 	*/
